4bbff22ac92a295e2d415eb227ed005f5e6f5e3c
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-07 19:03:53 +0300

    DAL Katmanında 1)Bağlantı cümlesi oluşturduk 2) Paket olarak sqlserveri kurduk 3) Entity katmanını Dal katmanına referans olarak verdik.

---
+++ AspNet_NTier_YazOkulu.DataAccessLayer/Baglanti.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace AspNet_NTier_YazOkulu.DataAccessLayer
+{
+    public class Baglanti
+    {
+        public static SqlConnection bgl = new SqlConnection("Data Source=DESKTOP-I1U4DR9;Initial Catalog=KahveSiparis;Integrated Security=True");
+    }
+}
